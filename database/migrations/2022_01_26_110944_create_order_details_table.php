66cf388f20f6a966c5acf9e3c6e14abbb072ffbc
Wern Ancheta <ancheta.wern@gmail.com>
2022-01-31 10:55:20 +0800

    add database migrations and factory

---
+++ database/migrations/2022_01_26_110944_create_order_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id');
+            $table->bigInteger('product_id');
+            $table->float('cost');
+            $table->integer('qty');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
