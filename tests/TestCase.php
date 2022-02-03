efa42f81090dcd36e891a15035a30fe6b1cdb541
Wern Ancheta <ancheta.wern@gmail.com>
2022-02-03 19:23:36 +0800

    add fix for RefreshDatabase not working properly

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,29 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+
+    public function tearDown(): void
+    {
+
+        $sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = 'food_order_test';";
+
+        DB::statement("SET FOREIGN_KEY_CHECKS = 0;");
+        $tables = DB::select($sql);
+
+        array_walk($tables, function($table){
+            if ($table->TABLE_NAME != 'migrations') {
+                DB::table($table->TABLE_NAME)->truncate();
+            }
+        });
+
+        DB::statement("SET FOREIGN_KEY_CHECKS = 1;");
+        parent::tearDown();
+    }
+
 }
